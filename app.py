a7e17c4b79a1cdc06266166a84bfa94c033a696d
vz415 <19888961+vz415@users.noreply.github.com>
2024-12-30 08:51:40 -0500

    fix the units

--- app.py
+++ app.py
@@ -109,10 +109,10 @@ for l_num in range(1, 6):
         type_i = type_i_rec[i - 1]
         for j in range(1, 4):
             type_ii = type_ii_rec[j - 1]
-            t_key = fr'K_{l_num}_{i}_{j}' #  [M$^{-2}$s$^{-1}$]
-            t_value = f'{ligand}-{type_i}-{type_ii}'
+            t_key = r'K_{l_num i j}' #  [M$^{-2}$s$^{-1}$]
+            t_value = f'{ligand}-{type_i}-{type_ii}  [M⁻²s⁻¹]'
             t_mappings[t_key] = t_value
-e_mappings = {f'e_{key[2:]}[a.u.]': value for key, value in t_mappings.items()}
+e_mappings = {f'e_{key[2:]}': f'{value} [a.u.]' for key, value in t_mappings.items()}
 combined_mappings = {**t_mappings, **e_mappings}
 
 # Interactive Streamlit App
